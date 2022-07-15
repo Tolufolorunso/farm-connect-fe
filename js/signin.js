1bf22d9d3bb878fa922b8cb4c3d3f638fb7cd115
Tolu Folorunso <tolufolorunso@yahoo.com>
2022-07-15 21:37:48 +0100

    Worked on login error

--- js/signin.js
+++ js/signin.js
@@ -1,20 +1,23 @@
+let error = document.querySelector('.error');
+let loader = document.querySelector('.loader');
+
 async function postData(event) {
   event.preventDefault();
-
-  let loader = document.querySelector('.loader');
-  loader.classList.remove('none');
+  error.textContent = '';
 
   let email = document.getElementById('email2').value;
   let password = document.getElementById('password1').value;
-  let error = document.querySelector('.error');
 
-  error.textContent = '';
+  if (!email || !password) {
+    error.classList.remove('none');
+    error.textContent = 'All fields are required';
+    return;
+  }
+
+  loader.classList.remove('none');
 
   // post body data
-  const user = {
-    email: email,
-    password: password,
-  };
+  const user = { email, password };
 
   try {
     const resp = await fetch(`${APIUrl}/users/login`, {
@@ -25,15 +28,10 @@ async function postData(event) {
       },
       body: JSON.stringify(user),
     });
-    if (!resp.ok) {
-      const msg = `There was an error "${resp.status} ${resp.statusText}"`;
-      throw new Error(msg);
-    }
 
     const res = await resp.json();
 
-    if (res.status === 'success') {
-      console.log(res.user);
+    if (res.status) {
       localStorage.setItem('farmconnectUser', res.token);
       localStorage.setItem('farmdata', JSON.stringify(res.user));
       if (res.user.role === 'investor') {
@@ -42,29 +40,28 @@ async function postData(event) {
         window.location.href = '/pages/farmers/f-dashboard.html';
       }
     } else {
-      if (res.data) {
-        loader.classList.add('none');
-        error.classList.remove('none');
-        error.textContent = res.data.error;
-      } else {
-        loader.classList.add('none');
-        error.classList.remove('none');
-        res.error.map((item) => {
-          let element = document.createElement('p');
-          element.textContent = item.message;
-          element.classList.add('error-item');
-          error.appendChild(element);
-        });
-      }
+      loader.classList.add('none');
+      throw new Error(res.data.error);
     }
   } catch (err) {
-    // console.log(err);
-    // console.log(60, err.message);
+    addError();
     let element = document.createElement('p');
     element.textContent = err.message;
     element.classList.add('error-item');
     error.appendChild(element);
+    setTimeout(() => {
+      removeError();
+    }, 3000);
   }
 }
+
+const removeError = () => {
+  error.classList.add('none');
+  error.textContent = '';
+};
+
+const addError = () => {
+  error.classList.remove('none');
+};
 
 document.getElementById('flogin').addEventListener('submit', postData);
